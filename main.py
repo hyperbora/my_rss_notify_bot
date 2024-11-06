f83047911118860169ba50a0db2f1950f5fe0ddd
hyperbora <hyperbora@gmail.com>
2024-11-06 22:56:11 +0900

    feat: /start 커맨드 추가

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from telegram.ext import ApplicationBuilder
+import asyncio
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import nest_asyncio
 from constants import BOT_TOKEN
 
 
-def main():
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello World")
+
+
+async def main():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    application.run_polling()
+
+    application.add_handler(CommandHandler("start", start))
+
+    await application.run_polling()
 
 
 if __name__ == "__main__":
     print("bot start")
-    main()
+    nest_asyncio.apply()
+    asyncio.run(main())

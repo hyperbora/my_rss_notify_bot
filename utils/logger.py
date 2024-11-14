0e5a8b8c0daba0fdc145f6ba1db8ea8dd8c9bee9
hyperbora <hyperbora@gmail.com>
2024-11-14 17:45:01 +0900

    feat: stderr, stdout도 로그파일에 기록하도록 수정

--- utils/logger.py
+++ utils/logger.py
@@ -3,6 +3,25 @@ import sys
 from logging.handlers import RotatingFileHandler
 from constants import LOG_FILE_PATH, MAX_LOG_SIZE, BACKUP_COUNT, LOG_LEVEL
 
+
+class StreamToLogger(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+
+    def __init__(self, st_logger, st_level):
+        self.logger = st_logger
+        self.level = st_level
+        self.linebuf = ""
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
+
+
 level = logging.getLevelNamesMapping().get(LOG_LEVEL, logging.INFO)
 
 # 로거 초기화
@@ -24,8 +43,5 @@ file_handler.setLevel(level)
 # 로거에 파일 핸들러 추가
 logger.addHandler(file_handler)
 
-# stderr 로그도 파일에 기록
-stderr_handler = logging.StreamHandler(sys.stderr)
-stderr_handler.setFormatter(formatter)
-stderr_handler.setLevel(logging.ERROR)
-logger.addHandler(stderr_handler)
+sys.stdout = StreamToLogger(logger, logging.INFO)
+sys.stderr = StreamToLogger(logger, logging.ERROR)

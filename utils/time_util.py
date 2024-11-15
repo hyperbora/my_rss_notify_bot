d1020648d070a8130d7defdc52c2dd1f10e6cba6
hyperbora <hyperbora@gmail.com>
2024-11-15 22:33:10 +0900

    fix: 최근 업데이트 시간 내역 사용자 시간대에 맞게 보여주도록 수정

--- utils/time_util.py
+++ utils/time_util.py
@@ -1,4 +1,5 @@
 import pytz
+from datetime import datetime
 
 TIMEZONE_MAP = {"en": "America/New_York", "ko": "Asia/Seoul"}
 
@@ -7,7 +8,11 @@ def get_user_timezone(language):
     return pytz.timezone(TIMEZONE_MAP.get(language, "UTC"))
 
 
-def format_date_for_user(date, language):
+def format_date_for_user(date: datetime, language):
     # UTC 시간대를 사용자 시간대로 변환
+    utc_timezone = pytz.timezone("UTC")
+    utc_time = utc_timezone.localize(date)
     user_timezone = get_user_timezone(language)
-    return date.astimezone(user_timezone).strftime("%Y-%m-%d %H:%M:%S")
+    localized_time = utc_time.astimezone(user_timezone)
+
+    return localized_time.strftime("%Y-%m-%d %H:%M:%S")
